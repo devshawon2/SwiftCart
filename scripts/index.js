4d8f8d2469bf7266fc96350c706e38509bec8c76
devshawon2 <upworkshawon260921@gmail.com>
2026-02-17 22:50:29 +0600

    top rated product section done

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,56 @@ const loadProducts = () => {
         .then((json) => displayProducts(json));
 };
 
+// Function for Top Rate Products
+const topRatedProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((json) => {
+            const topProductSorted = json.sort((a, b) => a.rating.rate - b.rating.rate);
+            const top3Products = topProductSorted.slice(0, 3);
+            console.log(top3Products);
+
+// top Product Container
+            let topProductsContainer = document.getElementById('trending-products');
+            topProductsContainer.innerHTML = ``;
+            top3Products.forEach(product => {
+                const topProductCard = document.createElement('div');
+                topProductCard.classList.add('card', 'bg-base-100', 'w-92', 'shadow-sm', 'border', 'border-indigo-400', 'py-4');
+                topProductCard.innerHTML = `
+                <figure>
+                    <img class="w-full h-48 object-contain" src="${product.image}" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <div class="gender-rating flex justify-between items-center">
+                        <div class="clothing-gender">
+                            <p class="gender text-primary open-sans-bold text-sm">${product.category}</p>
+                        </div>
+                        <div class="user-ratings flex items-center">
+                            <i class="fa-solid text-yellow-400 fa-star"></i>
+                            <p class="rating px-1"> ${product.rating.rate}</p>
+                            <p class="total-ratings"> (${product.rating.count})</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col h-[77px] relative">
+                    <h2 class="card-title product-name text-md line-clamp-2">${product.title}</h2>
+                    <br>
+                    <p class="product-price open-sans-bold text-lg absolute bottom-0 left-0">
+                        $<span>${product.price}</span>
+                    </p>
+                    </div>
+                    <div class="card-actions justify-around mt-auto">
+                        <button class="rounded-md px-12 btn btn-base text-gray-600"><i
+                                class="fa-regular fa-eye"></i>Details</button>
+                        <button class="rounded-md px-12 btn btn-primary"><i
+                                class="fa-solid fa-cart-shopping"></i>Add</button>
+                    </div>
+                </div>
+            `
+                topProductsContainer.append(topProductCard);
+            });
+        })
+}
+
 // click functionality for Categories
 document.addEventListener("click", function (event) {
     if (event.target.classList.contains("category-btn")) {
@@ -26,14 +76,18 @@ document.addEventListener("click", function (event) {
             button.classList.remove("bg-primary", "text-base-100");
         });
         btn.classList.add("bg-primary", "text-base-100");
-    }
-    const category = btn.dataset.category;
-    if (category === "all") {
-        loadProducts();
-    } else {
-        loadCategoryProduct(category);
+
+        const category = btn.dataset.category;
+        if (category === "all") {
+            loadProducts();
+        } else {
+            loadCategoryProduct(category);
+        }
     }
 });
+
+// Click Functionality for Details Buttons
+
 
 // categorizing products in groups
 loadCategoryProduct = async (category) => {
@@ -59,9 +113,8 @@ displayCategoryProduct = (data) => {
     const productsContainer = document.getElementById("products-container");
     productsContainer.innerHTML = ``;
     for (let product of data) {
-        console.log(product.title);
         const specificCards = document.createElement("div");
-        specificCards.classList.add('card', 'bg-base-100', 'w-auto', 'shadow-xl', 'flex', 'flex-wrap', 'pt-2', 'border', 'border-primary', 'h-md');
+        specificCards.classList.add('card', 'bg-base-100', 'w-auto', 'shadow-xl', 'flex', 'flex-wrap', 'pt-2', 'border', 'border-primary', 'h-sm');
         specificCards.innerHTML = `
         <figure>
                     <img class="w-full h-48 object-contain" src="${product.image}" alt="Shoes" />
@@ -98,7 +151,7 @@ displayCategoryProduct = (data) => {
 }
 // get and show Categories
 const displayCategories = (categories) => {
-    const categoriesContainer = document.getElementById("categories-container");
+    let categoriesContainer = document.getElementById("categories-container");
     for (let category of categories) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
@@ -148,10 +201,13 @@ const displayProducts = (products) => {
                 </div>
         `
         productsContainer.append(cardDiv);
-
     }
 }
 
+
+
+
 // Call Global Functions
+topRatedProducts();
 loadCategories();
 loadProducts();
